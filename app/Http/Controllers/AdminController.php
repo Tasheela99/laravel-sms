986ca71b034dc96ab55b935bea4062310a99eeb9
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-08-19 16:10:56 +0530

    init

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    {
+        return view('admin.dashboard');
+    }
+}
